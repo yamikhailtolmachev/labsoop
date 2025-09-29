b9a06afb862eff70c78cf00f7cbbcbe3fc8dcea2
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-09-29 22:24:09 +0400

    Исправлены тесты для класса IdentityFunction

--- lab2/src/test/java/functions/IdentityFunctionTest.java
+++ lab2/src/test/java/functions/IdentityFunctionTest.java
@@ -2,17 +2,15 @@ package functions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class IdentityFunctionTest {
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void apply() {
+    @Test
+    void testApply() {
+        IdentityFunction identityFunction = new IdentityFunction();
+        Assertions.assertEquals(101, identityFunction.apply(101));
+        Assertions.assertEquals(16.78, identityFunction.apply(16.78));
+        Assertions.assertEquals(0, identityFunction.apply(0));
     }
 }
