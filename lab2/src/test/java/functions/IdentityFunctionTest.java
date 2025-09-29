0b2d1c1089cccbbb7eaf697171300d8aabf06cab
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-09-29 21:17:59 +0400

    Создан класс IdentityFunction и покрыт тестами

---
+++ lab2/src/test/java/functions/IdentityFunctionTest.java
@@ -0,0 +1,18 @@
+package functions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityFunctionTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void apply() {
+    }
+}
