72002dade88459801d109416598c0a0bb1abdf6e
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-09-30 17:50:38 +0400

    Исправлены тесты для IdentityFunction

--- lab2/src/test/java/functions/IdentityFunctionTest.java
+++ lab2/src/test/java/functions/IdentityFunctionTest.java
@@ -9,7 +9,7 @@ class IdentityFunctionTest {
     @Test
     void testApply() {
         IdentityFunction identityFunction = new IdentityFunction();
-        Assertions.assertEquals(101, identityFunction.apply(101));
+        Assertions.assertEquals(107, identityFunction.apply(107));
         Assertions.assertEquals(16.78, identityFunction.apply(16.78));
         Assertions.assertEquals(0, identityFunction.apply(0));
     }
