8b795801a8fdee84c30f5ad8389b0066541e983f
DarkKorner <ildar2006aitov@gmail.com>
2025-09-30 19:20:32 +0300

    создан абстрактный класс AbstractTabulatedFunction

---
+++ lab2/src/main/java/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,30 @@
+package functions;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction {
+
+    protected abstract int floorIndexOfX(double x);
+    protected abstract double extrapolateLeft(double x);
+    protected abstract double extrapolateRight(double x);
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + (rightY - leftY) / (rightX - leftX) * (x - leftX);
+    }
+
+    @Override
+    public double apply(double x) {
+        if (x < leftBound()) {
+            return extrapolateLeft(x);
+        } else if (x > rightBound()) {
+            return extrapolateRight(x);
+        } else {
+            int index = indexOfX(x);
+            if (index != -1) {
+                return getY(index);
+            } else {
+                int floorIndex = floorIndexOfX(x);
+                return interpolate(x, floorIndex);
+            }
+        }
+    }
+}
