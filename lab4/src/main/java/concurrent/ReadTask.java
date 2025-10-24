00e756e6fbaac87d2ee6525255fe981149ce4a81
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-10-24 10:10:50 +0400

    Создал класс ReadTask, реализующий интерфейс Runnable

---
+++ lab4/src/main/java/concurrent/ReadTask.java
@@ -0,0 +1,20 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class ReadTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public ReadTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            double x = function.getX(i);
+            double y = function.getY(i);
+            System.out.printf("After read: i = %d, x = %f, y = %f\n", i, x, y);
+        }
+    }
+}
