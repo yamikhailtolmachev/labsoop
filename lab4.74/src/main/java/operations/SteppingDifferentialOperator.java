f2facd84c270f56bb7fa3f7f5db82ee81c0999c8
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:24:29 +0400

    Покрыл логами SteppingDifferentialOperator

--- lab4.74/src/main/java/operations/SteppingDifferentialOperator.java
+++ lab4.74/src/main/java/operations/SteppingDifferentialOperator.java
@@ -1,16 +1,21 @@
 package operations;
 
 import functions.MathFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class SteppingDifferentialOperator implements DifferentialOperator<MathFunction> {
+    private static final Logger logger = LoggerFactory.getLogger(SteppingDifferentialOperator.class);
 
     protected double step;
 
     public SteppingDifferentialOperator(double step) {
         if (step <= 0 || Double.isInfinite(step) || Double.isNaN(step)) {
+            logger.error("Недопустимый шаг: " + step);
             throw new IllegalArgumentException("Step must be positive finite number");
         }
         this.step = step;
+        logger.info("Создан SteppingDifferentialOperator с шагом " + step);
     }
 
     public double getStep() {
@@ -19,8 +24,10 @@ public abstract class SteppingDifferentialOperator implements DifferentialOperat
 
     public void setStep(double step) {
         if (step <= 0 || Double.isInfinite(step) || Double.isNaN(step)) {
+            logger.error("Недопустимый шаг при установке: " + step);
             throw new IllegalArgumentException("Step must be positive finite number");
         }
         this.step = step;
+        logger.info("Шаг изменен на " + step);
     }
 }
