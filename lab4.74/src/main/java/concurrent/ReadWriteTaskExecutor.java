d888fdb257a756a884ac715b6b39724db8a96725
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:46:01 +0400

    Покрыл логами ReadWriteTaskExecutor

--- lab4.74/src/main/java/concurrent/ReadWriteTaskExecutor.java
+++ lab4.74/src/main/java/concurrent/ReadWriteTaskExecutor.java
@@ -2,9 +2,15 @@ package concurrent;
 
 import functions.*;
 import functions.ConstantFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReadWriteTaskExecutor {
+    private static final Logger logger = LoggerFactory.getLogger(ReadWriteTaskExecutor.class);
+
     public static void main(String[] args) {
+        logger.info("Запуск ReadWriteTaskExecutor");
+
         ConstantFunction constantFunction = new ConstantFunction(-1);
 
         TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(
@@ -19,14 +25,20 @@ public class ReadWriteTaskExecutor {
         Thread readThread = new Thread(readTask);
         Thread writeThread = new Thread(writeTask);
 
+        logger.info("Запуск потоков чтения и записи");
+
         readThread.start();
         writeThread.start();
 
         try {
             readThread.join();
             writeThread.join();
+            logger.info("Оба потока завершили работу");
         } catch (InterruptedException e) {
+            logger.error("Прерывание потоков", e);
             e.printStackTrace();
         }
+
+        logger.info("Завершение ReadWriteTaskExecutor");
     }
 }
