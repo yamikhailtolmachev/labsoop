1ea7747a9290b41db7746813b5264863b02e6503
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:44:43 +0400

    Покрыл логами MultiplyingTaskExecutor

--- lab4.74/src/main/java/concurrent/MultiplyingTaskExecutor.java
+++ lab4.74/src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -5,14 +5,22 @@ import functions.TabulatedFunction;
 import functions.LinkedListTabulatedFunction;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MultiplyingTaskExecutor {
+    private static final Logger logger = LoggerFactory.getLogger(MultiplyingTaskExecutor.class);
+
     public static void main(String[] args) throws InterruptedException {
+        logger.info("Запуск MultiplyingTaskExecutor");
+
         UnitFunction unitFunction = new UnitFunction();
         TabulatedFunction function = new LinkedListTabulatedFunction(unitFunction, 1, 1000, 1000);
 
         List<Thread> threads = new ArrayList<>();
         int threadCount = 10;
+
+        logger.info("Создание " + threadCount + " потоков для умножения");
 
         for (int i = 0; i < threadCount; i++) {
             MultiplyingTask task = new MultiplyingTask(function);
@@ -26,6 +34,9 @@ public class MultiplyingTaskExecutor {
 
         Thread.sleep(2000);
 
+        logger.info("Вывод результата функции");
         System.out.println(function);
+
+        logger.info("Завершение MultiplyingTaskExecutor");
     }
 }
