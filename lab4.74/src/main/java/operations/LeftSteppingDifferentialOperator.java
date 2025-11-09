57e0874b659c5d8bd343011b8ff7f6cdc782241c
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:25:45 +0400

    Покрыл логами LeftSteppingDifferentialOperator

--- lab4.74/src/main/java/operations/LeftSteppingDifferentialOperator.java
+++ lab4.74/src/main/java/operations/LeftSteppingDifferentialOperator.java
@@ -1,15 +1,20 @@
 package operations;
 
 import functions.MathFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LeftSteppingDifferentialOperator extends SteppingDifferentialOperator {
+    private static final Logger logger = LoggerFactory.getLogger(LeftSteppingDifferentialOperator.class);
 
     public LeftSteppingDifferentialOperator(double step) {
         super(step);
+        logger.info("Создан LeftSteppingDifferentialOperator с шагом " + step);
     }
 
     @Override
     public MathFunction derive(MathFunction function) {
+        logger.info("Вычисление левой производной с шагом " + step);
         return new MathFunction() {
             @Override
             public double apply(double x) {
