0e88caa19a8157d8316a3bc3139e41e3b2aa2e18
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:42:58 +0400

    Покрыл логами LinkedListTabulatedFunctionFactory

--- lab4.74/src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
+++ lab4.74/src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -2,10 +2,15 @@ package functions.factory;
 
 import functions.LinkedListTabulatedFunction;
 import functions.TabulatedFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory {
+    private static final Logger logger = LoggerFactory.getLogger(LinkedListTabulatedFunctionFactory.class);
+
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
+        logger.info("Создание LinkedListTabulatedFunction через фабрику с " + xValues.length + " точками");
         return new LinkedListTabulatedFunction(xValues, yValues);
     }
 }
