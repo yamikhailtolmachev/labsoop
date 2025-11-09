04b1510ffc3fdf97ce5407ee4327c0f28a2c57ef
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:52:54 +0400

    Покрыл логами LinkedListTabulatedFunctionSerialization

--- lab4.74/src/main/java/io/LinkedListTabulatedFunctionSerialization.java
+++ lab4.74/src/main/java/io/LinkedListTabulatedFunctionSerialization.java
@@ -4,16 +4,22 @@ import functions.TabulatedFunction;
 import functions.LinkedListTabulatedFunction;
 import operations.TabulatedDifferentialOperator;
 import java.io.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LinkedListTabulatedFunctionSerialization {
+    private static final Logger logger = LoggerFactory.getLogger(LinkedListTabulatedFunctionSerialization.class);
 
     public static void main(String[] args) {
+        logger.info("Запуск LinkedListTabulatedFunctionSerialization");
         serializeFunctions();
-
         deserializeAndPrintFunctions();
+        logger.info("Завершение LinkedListTabulatedFunctionSerialization");
     }
 
     private static void serializeFunctions() {
+        logger.info("Сериализация LinkedListTabulatedFunction");
+
         try (
                 FileOutputStream fileStream = new FileOutputStream("output/serialized linked list functions.bin");
                 BufferedOutputStream bufferedStream = new BufferedOutputStream(fileStream)
@@ -25,19 +31,23 @@ public class LinkedListTabulatedFunctionSerialization {
             TabulatedDifferentialOperator differentialOperator = new TabulatedDifferentialOperator();
 
             TabulatedFunction firstDerivative = differentialOperator.derive(originalFunction);
-
             TabulatedFunction secondDerivative = differentialOperator.derive(firstDerivative);
 
             FunctionsIO.serialize(bufferedStream, originalFunction);
             FunctionsIO.serialize(bufferedStream, firstDerivative);
             FunctionsIO.serialize(bufferedStream, secondDerivative);
 
+            logger.info("Сериализация LinkedListTabulatedFunction завершена");
+
         } catch (IOException e) {
+            logger.error("Ошибка сериализации LinkedListTabulatedFunction", e);
             e.printStackTrace();
         }
     }
 
     private static void deserializeAndPrintFunctions() {
+        logger.info("Десериализация LinkedListTabulatedFunction");
+
         try (
                 FileInputStream fileStream = new FileInputStream("output/serialized linked list functions.bin");
                 BufferedInputStream bufferedStream = new BufferedInputStream(fileStream)
@@ -46,16 +56,17 @@ public class LinkedListTabulatedFunctionSerialization {
             TabulatedFunction deserializedFirstDerivative = FunctionsIO.deserialize(bufferedStream);
             TabulatedFunction deserializedSecondDerivative = FunctionsIO.deserialize(bufferedStream);
 
+            logger.info("Десериализация LinkedListTabulatedFunction завершена");
+
             System.out.println("Исходная функция:");
             System.out.println(deserializedOriginal.toString());
-
             System.out.println("Первая производная:");
             System.out.println(deserializedFirstDerivative.toString());
-
             System.out.println("Вторая производная:");
             System.out.println(deserializedSecondDerivative.toString());
 
         } catch (IOException | ClassNotFoundException e) {
+            logger.error("Ошибка десериализации LinkedListTabulatedFunction", e);
             e.printStackTrace();
         }
     }
