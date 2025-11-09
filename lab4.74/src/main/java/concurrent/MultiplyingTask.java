d642ba5fea6d680e77fd951bb0b2728b63e0011f
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:14:31 +0400

    Покрыл логами MultiplyingTask

--- lab4.74/src/main/java/concurrent/MultiplyingTask.java
+++ lab4.74/src/main/java/concurrent/MultiplyingTask.java
@@ -1,8 +1,11 @@
 package concurrent;
 
 import functions.TabulatedFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MultiplyingTask implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(MultiplyingTask.class);
     private final TabulatedFunction function;
 
     public MultiplyingTask(TabulatedFunction function) {
@@ -11,12 +14,16 @@ public class MultiplyingTask implements Runnable {
 
     @Override
     public void run() {
+        logger.info("Запуск MultiplyingTask в потоке " + Thread.currentThread().getName());
+
         for (int i = 0; i < function.getCount(); i++) {
             synchronized (function) {
                 double currentY = function.getY(i);
                 function.setY(i, currentY * 2);
             }
         }
+
+        logger.info("Поток " + Thread.currentThread().getName() + " завершил задачу");
         System.out.println("Thread " + Thread.currentThread().getName() + " completed the task");
     }
 }
