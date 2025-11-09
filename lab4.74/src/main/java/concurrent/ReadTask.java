b86be4da7b005c3f40068f5cb72cba48cc754083
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:17:26 +0400

    Покрыл логами ReadTask

--- lab4.74/src/main/java/concurrent/ReadTask.java
+++ lab4.74/src/main/java/concurrent/ReadTask.java
@@ -1,8 +1,11 @@
 package concurrent;
 
 import functions.TabulatedFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReadTask implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(ReadTask.class);
     private final TabulatedFunction function;
     private final Object lock;
 
@@ -13,12 +16,16 @@ public class ReadTask implements Runnable {
 
     @Override
     public void run() {
+        logger.info("Запуск ReadTask");
+
         for (int i = 0; i < function.getCount(); i++) {
             synchronized (lock) {
                 double x = function.getX(i);
                 double y = function.getY(i);
-                System.out.printf("After read: i = %d, x = %f, y = %f\n", i, x, y);
+                System.out.printf("After read: i = %d, x = %f, y = %f%n", i, x, y);
             }
         }
+
+        logger.info("Завершение ReadTask");
     }
 }
