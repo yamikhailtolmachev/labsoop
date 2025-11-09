bb6ac507f09ef44058e49ea1dac65d7203c15196
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:31:02 +0400

    Покрыл логами MiddleSteppingDifferentialOperator

--- lab4.74/src/main/java/operations/MiddleSteppingDifferentialOperator.java
+++ lab4.74/src/main/java/operations/MiddleSteppingDifferentialOperator.java
@@ -1,15 +1,20 @@
 package operations;
 
 import functions.MathFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MiddleSteppingDifferentialOperator extends SteppingDifferentialOperator {
+    private static final Logger logger = LoggerFactory.getLogger(MiddleSteppingDifferentialOperator.class);
 
     public MiddleSteppingDifferentialOperator(double step) {
         super(step);
+        logger.info("Создан MiddleSteppingDifferentialOperator с шагом " + step);
     }
 
     @Override
     public MathFunction derive(MathFunction function) {
+        logger.info("Вычисление центральной производной с шагом " + step);
         return new MathFunction() {
             @Override
             public double apply(double x) {
