6f8189809661b91a64297493db3427ccf07703e3
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-11-09 11:41:42 +0400

    Покрыл логами ArrayTabulatedFunctionFactory

--- lab4.74/src/main/java/functions/factory/ArrayTabulatedFunctionFactory.java
+++ lab4.74/src/main/java/functions/factory/ArrayTabulatedFunctionFactory.java
@@ -2,10 +2,15 @@ package functions.factory;
 
 import functions.ArrayTabulatedFunction;
 import functions.TabulatedFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ArrayTabulatedFunctionFactory implements TabulatedFunctionFactory {
+    private static final Logger logger = LoggerFactory.getLogger(ArrayTabulatedFunctionFactory.class);
+
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
+        logger.info("Создание ArrayTabulatedFunction через фабрику с " + xValues.length + " точками");
         return new ArrayTabulatedFunction(xValues, yValues);
     }
 }
