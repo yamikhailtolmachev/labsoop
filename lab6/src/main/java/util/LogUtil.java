1c066b0bf5532acfb8487cd631de0877119297ab
DarkKorner <ildar2006aitov@gmail.com>
2025-12-21 23:28:04 +0300

    Исправлены ошибки

--- lab6/src/main/java/util/LogUtil.java
+++ lab6/src/main/java/util/LogUtil.java
@@ -13,6 +13,7 @@ public class LogUtil {
     private static final String AUTH_LOG_FILE = LOG_DIR + "/auth.log";
     private static final String ACCESS_LOG_FILE = LOG_DIR + "/access.log";
     private static final String ERROR_LOG_FILE = LOG_DIR + "/error.log";
+    private static final String APP_LOG_FILE = LOG_DIR + "/application.log";
 
     static {
         new File(LOG_DIR).mkdirs();
@@ -24,6 +25,7 @@ public class LogUtil {
 
         System.out.println(logMessage);
         writeToFile(AUTH_LOG_FILE, logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
     }
 
     public static void logAccess(String message) {
@@ -32,6 +34,7 @@ public class LogUtil {
 
         System.out.println(logMessage);
         writeToFile(ACCESS_LOG_FILE, logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
     }
 
     public static void logError(String message) {
@@ -40,6 +43,7 @@ public class LogUtil {
 
         System.err.println(logMessage);
         writeToFile(ERROR_LOG_FILE, logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
     }
 
     public static void logError(String message, Throwable throwable) {
@@ -47,7 +51,41 @@ public class LogUtil {
         String logMessage = String.format("[%s] [ERROR] %s: %s", timestamp, message, throwable.getMessage());
 
         System.err.println(logMessage);
+        throwable.printStackTrace();
         writeToFile(ERROR_LOG_FILE, logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
+    }
+
+    public static void info(String message) {
+        String timestamp = DATE_FORMAT.format(new Date());
+        String logMessage = String.format("[%s] [INFO] %s", timestamp, message);
+
+        System.out.println(logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
+    }
+
+    public static void warn(String message) {
+        String timestamp = DATE_FORMAT.format(new Date());
+        String logMessage = String.format("[%s] [WARN] %s", timestamp, message);
+
+        System.out.println(logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
+    }
+
+    public static void error(String message) {
+        logError(message);
+    }
+
+    public static void error(String message, Throwable throwable) {
+        logError(message, throwable);
+    }
+
+    public static void debug(String message) {
+        String timestamp = DATE_FORMAT.format(new Date());
+        String logMessage = String.format("[%s] [DEBUG] %s", timestamp, message);
+
+        System.out.println(logMessage);
+        writeToFile(APP_LOG_FILE, logMessage);
     }
 
     private static void writeToFile(String filename, String message) {
